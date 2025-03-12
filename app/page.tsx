933549009151530f94091c61a94d6e8b867c11b5
Martin Andreev <martin_i_a@abv.bg>
2025-03-12 18:46:26 +0200

    Change the app font, bring some necessary files, and delete other which won't be necessary

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const Homepage = () => {
+  return <>Prostore</>;
+};
+
+export default Homepage;
