00626d99a4b64a70f00fbb785a8942e486153311
Martin Andreev <martin_i_a@abv.bg>
2025-03-17 20:23:50 +0200

    Add `useTransition` hook to handle the state of the buttons
    Now if we click on the "add to cart" button or on the - or + button, a spinner is appearing on its place.

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,42 +1,49 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, Loader } from "lucide-react";
 import { Cart, CartItem } from "@/types";
 import { toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { useTransition } from "react";
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
 
+  const [isPending, startTransition] = useTransition();
+
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res.success) {
-      toast.error(res.message);
+      if (!res.success) {
+        toast.error(res.message);
 
-      return;
-    }
-    // Handle success add to cart
-    toast.success(res.message, {
-      action: {
-        label: "Go to Cart",
+        return;
+      }
+      // Handle success add to cart
+      toast.success(res.message, {
+        action: {
+          label: "Go to Cart",
 
-        onClick: () => router.push("/cart"),
-      },
+          onClick: () => router.push("/cart"),
+        },
+      });
     });
   };
   // Handle remove from cart
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    if (res.success) {
-      toast(res.message);
-    } else {
-      toast.error(res.message);
-    }
+      if (res.success) {
+        toast(res.message);
+      } else {
+        toast.error(res.message);
+      }
 
-    return;
+      return;
+    });
   };
   // Check if item is in cart
   const existItem =
@@ -45,16 +52,28 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="w-4 h-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
       <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="w-4 h-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
       </Button>
     </div>
   ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus className="w-4 h-4" />
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="w-4 h-4" />
+      )}{" "}
       Add To Cart
     </Button>
   );
